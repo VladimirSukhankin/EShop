2d914c9aaf580bf106622f037dbc972fae3f2fb6
Суханкин Владимир <v.sukhankin@kmd24.com>
2024-03-14 23:49:10 +0400

    добавление энтити

---
+++ EShopFanerum.Persistance/Migrations/20240306183849_AddTableShopCarts.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EShopFanerum.Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTableShopCarts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
